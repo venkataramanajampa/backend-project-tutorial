2d65f46b329cba59962ea727f234a259bbd50751
venkataramanajampa <venkataramanajampa18@gmail.com>
2024-07-06 16:44:30 +0530

    util code discusiion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/index.js";
-const app = express();
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`server listening at ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mango db connection failed !!!!", error);
+  });
+
 // (async () => {
 //   try {
 //     mongoose.connect(`${process.env.MANGO_URI}/${DB_Name}`);
