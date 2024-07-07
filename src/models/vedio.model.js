072e1e15e50f126a98ac53415214e145eef6efb8
venkataramanajampa <venkataramanajampa18@gmail.com>
2024-07-07 10:06:01 +0530

    bycrypting and jwt tokens

---
+++ src/models/vedio.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const vedioSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+vedioSchema.plugin(mongooseAggregatePaginate);
+
+export const Vedio = mongoose.model("Vedio", vedioSchema);
